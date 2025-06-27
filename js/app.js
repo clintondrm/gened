38551ab7dccdca8689526c369fd428dc5f203acc
Clinton Daniel Roeth-McKay <clintondrm@gmail.com>
2025-06-27 16:09:54 -0400

    Fetch courses from gened-courses.json

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
 import { initInterface } from './interface.js';
 
+async function loadCourses() {
+  const courses = await fetch('gened-data/gened-courses.json').then(r => r.json());
+  const first50 = courses.slice(0, 50);
+  const container = document.querySelector('#course-list');
+  container.innerHTML = renderCourses(first50);
+}
+
+function renderCourses(courses) {
+  let html = '<ul class="rvt-list-plain">';
+  courses.forEach(c => {
+    html += `<li>${c.subj} ${c.nbr}: ${c.desc}</li>`;
+  });
+  html += '</ul>';
+  return html;
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   initInterface();
+  loadCourses();
 });
